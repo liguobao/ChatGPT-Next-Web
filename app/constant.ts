c6360a61d92dfc8fc401058e31db7c132351cb7c
liguobao <codelover@qq.com>
2024-04-22 10:08:02 +0800

    feat: remove path

--- app/constant.ts
+++ app/constant.ts
@@ -72,9 +72,6 @@ export const EXPORT_MESSAGE_CLASS_NAME = "export-markdown";
 
 export enum ServiceProvider {
   OpenAI = "OpenAI",
-  Azure = "Azure",
-  Google = "Google",
-  Anthropic = "Anthropic",
 }
 
 export enum ModelProvider {
